fad78051fd835025b3b4d6c5581f8b308c237722
Cojate <cjtessler1@gmail.com>
2013-05-16 22:52:53 -0400

    added hangman.py

--- hangman.py
+++ hangman.py
@@ -1,44 +1,5 @@
-# Tell player 2 to look away
-
-# Ask player 1 for a word
-
-# Ask for a letter
-
-# If letter matches any letter in the string
-
-# If no, You draw hangman with new part
-
-# If yes, print word with values in spot
-
-# Checks is player 2 as won
-
-# Add option to to guess word 
-
 import string
 from os import system
-
-
-# global variable to keep track of misses
-#misses = 0
-
-""" 
-#punc = []
-#for symbol in string.punctuation:
-#	punc.append(symbol)
-
-
-# To be implemented later 
-
-def punc_check(phrase)
-test = False
-while test == False:
-	word = raw_input(prompt)
-	for letter in word:
-		if letter in punc:
-			print "No symbols please."
-		else:
-			test = True
-"""
 
 def prepare(phrase):
 	array = []
@@ -56,29 +17,6 @@ def blanks(letters):
 		else:
 			guess_array.append("-")
 	return guess_array
-
-
-#"""
-#def misses():
-#	global misses
-#	misses = misses + 1
-#
-#	if misses == 1:
-#		print """
-#Head
-#		"""
-#	if misses == 2:
-#		print """
-#Body
-#		"""
-#
-#	if misses == 3:
-#		print """
-#arms
-#		"""
-#"""
-
-
 
 def guess():
 	
@@ -103,12 +41,7 @@ def guess():
 	for i in range(len(word_array)):
 		if guess == word_array[i]:
 			guess_array[i] = guess
-
-	#if guess_array == check_array:
-	#	misses()
-
 	print "".join(guess_array)
-
 
 
 if __name__=="__main__":
@@ -128,17 +61,4 @@ if __name__=="__main__":
 	while "-" in guess_array:
 		guess()
 		system('clear')
-
-
-
-
-
-
-
-
-
-
-
-
-
 
